80e4b91bd402553482de0fafd87f5231f8279841
Angel Resendiz <angel.jrl@hotmail.com>
2024-02-16 12:07:31 -0600

    Agregado archivo .gitignore y cambios en archivos de configuración

--- admin/config/config.php
+++ admin/config/config.php
@@ -3,7 +3,7 @@
     define('HOST','localhost');
     define('DB','ceunem');
     define('USER','root');
-    define('PASSWORD','');
+    define('PASSWORD','@Ngel199616');
     define('CHARSET','utf8');
 
     define('PUBLIC', 'http://localhost/ceunem/public/');
